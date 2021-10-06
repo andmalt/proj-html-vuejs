e145552f2c5a3f2ac1a11022e9006d14334ef0b1
andmalt <andrea.maltempi@gmail.com>
2021-10-06 12:26:15 +0200

    inserted principal components and images

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+
+  </footer>
+</template>
+
+<script>
+export default {
+    name:'Footer',
+    props: {
+
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+    name:'Header',
+    props: {
+
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/general.scss';
+@import '../style/variables.scss';
+
+
+</style>
