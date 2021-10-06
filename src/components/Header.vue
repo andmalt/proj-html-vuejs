e738a71b5a5e29af4951fb12d07fc9530868fa32
andmalt <andrea.maltempi@gmail.com>
2021-10-06 14:51:45 +0200

    inserted logo and fas

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,26 @@
 <template>
-  <header>
+    <header class="container-fluid" id="bg">
+        <!-- logo and icons -->
+        <div class="row">
+            <div class="col-6 text-center py-4 logo_h">
+                <img src="../assets/img/avadabarbers-logo-x1.png" alt="logo Avada Barbers">
+            </div>
 
-  </header>
+            <div class="col-6 d-flex justify-content-center align-items-center py-4">
+                <a href="#shopping"><i class="fas fa-shopping-cart"></i></a>
+                <a href="#menu"><i class="fas fa-bars"></i></a>
+            </div>
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+
+        </div>
+
+    </header>
 </template>
 
 <script>
@@ -17,5 +36,25 @@ export default {
 @import '../style/general.scss';
 @import '../style/variables.scss';
 
+#bg{
+    background-image: url(../assets/img/avadabarbers-homepage-hero-bg.jpg);
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 600px;
+    .fas{
+        color: white;
+        margin-left: 1.2rem;
+        font-size: 0.9rem;
+    }
+    .fa-bars{
+            font-size: 1.5rem;
+        }
+    .logo_h{
+        img{
+            height: 3.2rem;
+        }
+    }
+}
 
 </style>
