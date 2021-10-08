560773a229f66ec199d66166c4622a54b29cf40c
andmalt <andrea.maltempi@gmail.com>
2021-10-08 13:08:33 +0200

    inserted main footer section

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,24 +1,30 @@
 <template>
-  <footer class="container-fluid text-center py-2" id="footer">
+  <footer class="container-fluid text-center py-1" id="footer">
     <!-- main section -->
-    <section class="row justify-content-center py-5" id="f-m">
+    <section class="row justify-content-center pt-3 pb-5" id="f-m">
 
       <div class="col-3 f-img pb-2">
         <img src="../assets/img/avadabarbers_footer_shop_image-700x546.png" alt="image products Avada Barbers">
         <a href="#">SHOP OUR RANGE NOW</a>
       </div>
 
-      <div class="col-3 f-img pb-2">
-        <div>
+      <div class="col-3 p-2">
+        
+        <div class="f-img-l mb-5">
+          <!-- logo -->
+          <img src="../assets/img/avadabarbers-logo-x1.png" alt="logo Avada Barbers">
 
+          <p>Avada Barbers 123 <br> New York Street <br> New York City <br> info@yourwebsite.com <br> +1(555)555-1212</p>
         </div>
 
-        <div>
-
-        </div>
-
-        <div>
-
+        <div id="socials">
+          <h6>FOLLOW US</h6>
+          <ul class="d-flex justify-content-center">
+            <li><a href="#facebook" class="fab fa- fa-facebook"></a></li>
+            <li><a href="#twitter" class="fab fa- fa-twitter"></a></li>
+            <li><a href="#youtube" class="fab fa- fa-youtube"></a></li>
+            <li><a href="#instagram" class="fab fa- fa-instagram"></a></li>
+          </ul>
         </div>
       </div>
 
@@ -60,6 +66,34 @@ export default {
       border:2px solid $refColor;
       color: $refColor;
       text-decoration: none;
+    }
+  }
+  .f-img-l{
+    padding: 0 4rem;
+    img{
+      height: 3rem;
+      margin-bottom: 1.5rem;
+    }
+    p{
+      color: $titles;
+    }
+  }
+}
+#socials{
+  h6{
+    color: $refColor;
+    font-weight: 600;
+  }
+  ul{
+    padding: 0;
+
+    li{
+      margin: 0.5rem;
+      list-style: none;
+      a{
+        color: $titles;
+        text-decoration: none;
+      }
     }
   }
 }
